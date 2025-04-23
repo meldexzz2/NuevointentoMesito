c6594a544d048e0ea100cfebfeeb1f9214f18a30
Wichi <sebastianbarbaro82@gmail.com>
2025-04-23 11:18:44 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ import cfonts from 'cfonts';
 import { createInterface } from 'readline';
 import yargs from 'yargs';
 import chalk from 'chalk';
-console.log('\n✰ Iniciando BarbozaBot ✰');
+console.log('\n✰ Iniciando BolilloBot ✰');
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname);
 const { name, description, author, version } = require(join(__dirname, './package.json'));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
-say('BotBarboza\nBot', {
+say('bot bolillo\nai', {
 font: 'block',
 align: 'center',
 colors: ['white']
@@ -23,7 +23,7 @@ font: 'chrome',
 align: 'center',
 colors: ['red']
 });
-say(`Developed By • Barboza Ai`, {
+say(`Developed By • Bot bolillo`, {
 font: 'console',
 align: 'center',
 colors: ['magenta']
