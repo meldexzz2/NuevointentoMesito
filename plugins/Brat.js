df0481fd724d391312555d1872b1bc4346f5d899
meldexzz2 <jos7.xx@gmail.com>
2025-04-23 14:19:17 -0600

    Create Brat.js

---
+++ plugins/Brat.js
@@ -0,0 +1,27 @@
+import fetch from 'node-fetch';
+
+const handler = async (m, { conn, args, usedPrefix, command }) => {
+    try {
+        if (!args[0]) {
+            return m.reply(`❌ Ingresa el texto para el sticker.\n\nEjemplo: *${usedPrefix + command} Perra*`);
+        }
+
+        const text = encodeURIComponent(args.join(" "));
+        const apiUrl = `https://api.siputzx.my.id/api/m/brat?text=${text}`;
+
+        await conn.sendMessage(m.chat, { react: { text: '🎨', key: m.key } });
+
+        await conn.sendMessage(m.chat, {
+            sticker: { url: apiUrl }
+        }, { quoted: m });
+
+        await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key } });
+
+    } catch (err) {
+        console.error(err);
+        m.reply(`❌ Ocurrió un error al generar el sticker.`);
+    }
+};
+
+handler.command = /^brat$/i;
+export default handler;
