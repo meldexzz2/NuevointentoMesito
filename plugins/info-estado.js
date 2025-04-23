0b8732d81d81b75e1380675186bf1d0da8eb0f41
Wichi <sebastianbarbaro82@gmail.com>
2025-04-23 10:22:11 -0400

    Actualizar info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, isRowner }) => {
     let _muptime;
     let totalreg = Object.keys(global.db.data.users).length;
     let totalchats = Object.keys(global.db.data.chats).length;
-    let pp = 'https://qu.ax/Mvhfa.jpg'; // Usamos la URL de la imagen aquí
+    let pp = 'https://i.postimg.cc/Vv73j0HY/IMG-6032.jpg'; // Usamos la URL de la imagen aquí
 
     if (process.send) {
         process.send('uptime');
@@ -17,8 +17,8 @@ let handler = async (m, { conn, isRowner }) => {
     const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats);
     const groupsIn = chats.filter(([id]) => id.endsWith('@g.us'));
     const used = process.memoryUsage();
-    let Sisked = `╭─⬣「 *Estado De Bot Barboza* 」⬣\n`;
-    Sisked += `│ 👤 *Creador ∙* Barboza\n`;
+    let Sisked = `╭─⬣「 *𝙈𝙚𝙨𝙞𝙩𝙤𝘽𝙤𝙩.🦥* 」⬣\n`;
+    Sisked += `│ 👤 Creador ∙𝙈𝙚𝙨𝙞𝙩𝙤𝘽𝙤𝙩.🦥\n`;
     Sisked += `│ 💎 *Grupos Unidos ∙* ${groupsIn.length}\n`;
     Sisked += `│ 💨 *Chats Privados ∙* ${chats.length - groupsIn.length}\n`;
     Sisked += `│ 🪙 *Total De Chats ∙* ${chats.length}\n`;
