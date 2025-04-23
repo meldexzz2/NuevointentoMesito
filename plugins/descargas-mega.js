61797d7be09a445457764af60f1a7842172e4124
Wichi <sebastianbarbaro82@gmail.com>
2025-04-23 10:29:34 -0400

    Actualizar descargas-mega.js

--- plugins/descargas-mega.js
+++ plugins/descargas-mega.js
@@ -16,7 +16,7 @@ let handler = async (m, { conn, args, usedPrefix, text, command }) => {
         // Reemplazamos rwait con una reacción válida, como una marca de verificación
         m.react('✅'); // Puedes personalizar el emoji según prefieras
 
-        const caption = `   *--- ${botName} ---*\nFile: ${file.name}\nSize: ${formatBytes(file.size)}\n> ৎ୭࠭͢Bot Barboza Súper Bot𓆪͟͞ `;
+        const caption = `   *--- ${botName} ---*\nFile: ${file.name}\nSize: ${formatBytes(file.size)}\n> ৎ୭࠭͢𝙈𝙚𝙨𝙞𝙩𝙤𝘽𝙤𝙩.🦥 `;
 
         const data = await file.downloadBuffer();
 
