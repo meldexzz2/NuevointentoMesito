bc4e46f4ddc4b23f43935e52d6680fda52d7c355
meldexzz <140272375+meldexzz@users.noreply.github.com>
2025-04-23 17:49:39 -0600

    Create stock.js

---
+++ plugins/stock.js
@@ -0,0 +1,12 @@
+let handler = async (m, { conn }) => {
+    let chat = global.db.data.chats[m.chat]
+    if (chat.setstock) {
+        let stock = chat.setstock;
+        await conn.reply(m.chat, stock, m);
+    } else {
+        m.reply(`𝙉𝙤 𝙨𝙚 𝙝𝙖 𝙚𝙨𝙩𝙖𝙗𝙡𝙚𝙘𝙞𝙙𝙤 𝙪𝙣 𝙘𝙖𝙩á𝙡𝙤𝙜𝙤 𝙥𝙖𝙧𝙖 𝙎𝙩𝙤𝙘𝙠, 𝙪𝙩𝙞𝙡𝙞𝙯𝙖 .𝙨𝙚𝙩𝙨𝙩𝙤𝙘𝙠 𝙥𝙖𝙧𝙖 𝙚𝙨𝙩𝙖𝙗𝙡𝙚𝙘𝙚𝙧 𝙪𝙣𝙤.`);
+    }
+}
+handler.command = ['stock'];
+handler.group = true;
+export default handler;
