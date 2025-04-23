f58d070956e70914ca965ad590180cf373fdb880
Wichi <sebastianbarbaro82@gmail.com>
2025-04-23 10:33:26 -0400

    Actualizar _audio.js

--- plugins/_audio.js
+++ plugins/_audio.js
@@ -147,7 +147,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
       `💎 *Nombre:* ${name}\n` +
       `🗣️ *Artista:* ${artist}\n` +
       `⏱️ *Duración:* ${duration}\n\n` +
-      `> @Barboza - Bot - 𝟢𝟨`,
+      `> @𝙈𝙚𝙨𝙞𝙩𝙤𝘽𝙤𝙩.🦥`,
     contextInfo: {
       externalAdReply: {
         title: name,
